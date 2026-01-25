789bc2efbf4ed14d095825ee9bd8fa3f900caa03
andresg00 <hagc1995@gmail.com>
2026-01-25 10:27:45 -0500

    feat: reorganizar inicialización de propiedades en MediaFile y mejorar conversión a objeto plano

--- conmon/models/media.js
+++ conmon/models/media.js
@@ -4,12 +4,14 @@ class MediaFile {
     this.id = data.id || data.asset_id || "";
     this.source = data.source || data.url || "";
     this.type = data.resourceType || data.type || "application/octet-stream";
-    this.width = data.width || 0;
-    this.height = data.height || 0;
     this.size = data.size || data.bytes || 0;
-    this.thumb = data.thumb?.url || data.thumb || null;
-    this.thumbs400 = data.thumbs400?.url || data.thumbs400 || null;
-    this.thumbs800 = data.thumbs800?.url || data.thumbs800 || null;
+    if (this.type.startsWith("image/") || this.type.startsWith("video/")) {
+      this.width = data.width || 0;
+      this.height = data.height || 0;
+      this.thumb = data.thumb?.url || data.thumb || null;
+      this.thumbs400 = data.thumbs400?.url || data.thumbs400 || null;
+      this.thumbs800 = data.thumbs800?.url || data.thumbs800 || null;
+    }
     // this.medium = data.medium?.url || data.medium || null;
     this.createdAt = data.time
       ? new Date(data.time)
@@ -26,25 +28,27 @@ class MediaFile {
 
   // Convertir a objeto plano
   toMap() {
+    // Opción 1: Poner un valor nulo si la fecha es inválida
     const validDate =
-      this.createdAt instanceof Date && !isNaN(this.createdAt)
+      this.createdAt instanceof Date && !isNaN(this.createdAt.getTime())
         ? this.createdAt.toISOString()
         : new Date().toISOString();
-    return {
-      id: this.id,
-      source: this.source,
-      type: this.type,
-      width: this.width,
-      height: this.height,
-      size: this.size,
-      thumb: this.thumb,
-      thumbs400: this.thumbs400,
-      thumbs800: this.thumbs800,
-      // medium: this.medium,
-      createdAt: validDate,
-      ext: this.ext,
-      // ...this.extra, // todos los campos extras
-    };
+    const map = {};
+
+    if (this.source) map.source = this.source;
+    if (this.type) map.type = this.type;
+    if (this.width) map.width = this.width;
+    if (this.height) map.height = this.height;
+    if (this.size) map.size = this.size;
+    if (this.thumb) map.thumb = this.thumb;
+    if (this.thumbs400) map.thumbs400 = this.thumbs400;
+    if (this.thumbs800) map.thumbs800 = this.thumbs800;
+    if (this.ext) map.ext = this.ext;
+    if (this.id) map.id = this.id;
+
+    map.createdAt = validDate;
+
+    return map;
   }
 
   // Crear instancia desde un mapa/JSON
